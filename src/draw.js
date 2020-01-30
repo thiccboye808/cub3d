cb47bd6da4d6ad7d955bdaa17e0977118d7b810b
thicc-boye1 <thicc-boye1@users.noreply.github.com>
2020-01-30 02:04:21 -0500

    updated to cooler pattern

--- src/draw.js
+++ src/draw.js
@@ -8,11 +8,14 @@ function draw()
     {
       for( let x = 0; x < 20; x ++ )
       {
-        if (x == 0 || y == 0 || z == 20 )
-        image( cube, 
-          ( x * CUBE_TILE_XX ) + ( y * CUBE_TILE_YX ) - camera.x, 
-          ( x * CUBE_TILE_XY ) + ( y * CUBE_TILE_YY ) + ( z * CUBE_TILE_YZ ) - camera.y );
+        // imma just use a shitty pattern but it looks cool o.O
+        if( x == 15 || x == 5 || y == 15 || y == 5 || z == 15 || z == 5 )
+        {
+          image( cube, 
+            ( x * CUBE_TILE_XX ) + ( y * CUBE_TILE_YX ) - camera.x, 
+            ( x * CUBE_TILE_XY ) + ( y * CUBE_TILE_YY ) + ( z * CUBE_TILE_YZ ) - camera.y );
+        }
       }
     }
-    }
+  }
 }
