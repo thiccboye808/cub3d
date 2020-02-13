103b0c8b47885227c406e47815dc3039634a6a17
thiccboye808 <thiccboye808@gmail.com>
2020-02-12 23:51:01 -0500

    bridge + console api update

--- debug/edit.js
+++ debug/edit.js
@@ -5,46 +5,48 @@
 
 let edit =
 {
-  cube: ( x, y, z, c ) => // adds cube, returns cube
+  add: ( x, y, z, s ) => // adds sprite, returns sprite
   {
+    if( typeof s === "string" )
+      s = sprite.sprites[ s.valueOf() ];
     x = Math.round( x );
     y = Math.round( y );
     z = Math.round( z );
-    map.array[ x ][ y ][ z ] = c;
-    console.log( `edited cube at (${x}, ${y}, ${z}) to ${c}` );
-    return c;
+    map.array[ x ][ y ][ z ] = s;
+    console.log( `edited cube at (${x}, ${y}, ${z}) to ${s}` );
+    return s;
   },
-  line: ( x1, y1, z1, x2, y2, z2, c ) => // adds a line cubes from 2 points, returns cube 
+  line: ( x1, y1, z1, x2, y2, z2, s ) => // adds a line of sprites from 2 points, returns sprite 
   {
     if( x1 != x2 )
-      for( let x = Math.min( x1, x2 ); x <= Math.max( x1, x2 ); x++ ) 
+      for( let x = Math.min( x1, x2 ); x <= Math.max( x1, x2 ); x ++ ) 
       {
         let y = ( ( y2 - y1 ) / ( x2 - x1 ) * ( x - x1 ) ) + y1;
         let z = ( ( z2 - z1 ) / ( x2 - x1 ) * ( x - x1 ) ) + z1;
-        edit.cube( x, y, z, c );
+        edit.add( x, y, z, s );
       }
     else if( y1 != y2 )
-      for( let y = Math.min( y1, y2 ); y <= Math.max( y1, y2 ); y++ ) 
+      for( let y = Math.min( y1, y2 ); y <= Math.max( y1, y2 ); y ++ ) 
       {
         let x = ( ( x2 - x1 ) / ( y2 - y1 ) * ( y - y1 ) ) + x1;
         let z = ( ( z2 - z1 ) / ( y2 - y1 ) * ( y - y1 ) ) + z1;
-        edit.cube( x, y, z, c );
+        edit.add( x, y, z, s );
       }
     else if( z1 != z2 )
-      for( let z = Math.min( z1, z2 ); z <= Math.max( z1, z2 ); z++ ) 
+      for( let z = Math.min( z1, z2 ); z <= Math.max( z1, z2 ); z ++ ) 
       {
         let x = ( ( x2 - x1 ) / ( z2 - z1 ) * ( z - z1 ) ) + x1;
         let y = ( ( y2 - y1 ) / ( z2 - z1 ) * ( z - z1 ) ) + y1;
-        edit.cube( x, y, z, c );
+        edit.add( x, y, z, s );
       }
-    return c;
+    return s;
   },
-  fill: ( x, y, z, xl, yl, zl, c ) =>
+  fill: ( x, y, z, xl, yl, zl, s ) =>
   {
-    for( let zc = z; zc <= z + zl; zc ++ )
-      for( let xc = x; xc <= x + xl; xc ++ )
-        for( let yc = y; yc <= y + yl; yc++ )
-            edit.cube( xc, yc, zc, c );
-    return c;
+    for( let zs = z; zs <= z + zl; zs ++ )
+      for( let xs = x; xs <= x + xl; xs ++ )
+        for( let ys = y; ys <= y + yl; ys ++ )
+          edit.add( xs, ys, zs, s );
+    return s;
   }
 }
